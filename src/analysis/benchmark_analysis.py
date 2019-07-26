6d4b87daf61dbc455e7fd693a7eed1f6e94f5360
liolau <xxliolauxx@gmail.com>
2019-07-26 15:21:18 +0200

    Added full queue plot for reference

--- src/analysis/benchmark_analysis.py
+++ src/analysis/benchmark_analysis.py
@@ -14,21 +14,27 @@ class BenchmarkAnalysis():
 
 	def run(self):
 		solo = self.data.query('scheme_a==scheme_b')
-		self.plot_loss(			'Lossrate vs Queue Size', 						'loss.png', 			solo)
-		self.plot_variance(		'Relative Standard Deviation vs Queue Size', 	'rsd.png', 				solo)
-		self.plot_fair(			'Jain Fairness vs RTT Unfairness', 				'fairness.png', 		solo)
-		self.plot_fair_total(	'Jain Fairness (total) vs RTT Unfairness', 		'fairness_total.png',	solo)
-		self.plot_time_to_convergence('Convergence Time vs Fairness after Convergence',		'convergence.png',		solo)
-		self.plot_queueing_delay('Queuing Delay vs Queue Size', 				'delay.png',			solo)
+		scheme_a = solo['scheme_a'].values[0]
+		scheme_b = solo['scheme_b'].values[0]
+		scheme_str = "\n6x%s competing"%(scheme_a)
+		self.plot_loss(			'Lossrate vs Queue Size' + scheme_str, 								'loss.png', 			solo)
+		self.plot_variance(		'Relative Standard Deviation vs Queue Size' + scheme_str, 			'rsd.png', 				solo)
+		self.plot_fair(			'Jain Fairness vs RTT Unfairness' + scheme_str, 					'fairness.png', 		solo)
+		self.plot_fair_total(	'Jain Fairness (total) vs RTT Unfairness' + scheme_str, 			'fairness_total.png',	solo)
+		self.plot_time_to_convergence('Convergence Time vs Fairness after Convergence' + scheme_str,'convergence.png',		solo)
+		self.plot_queueing_delay('Queuing Delay vs Queue Size' + scheme_str, 						'delay.png',			solo)
 
 		mixed = self.data.query('scheme_a!=scheme_b')
 		if len(mixed)==0: return #in case of cubic
-		self.plot_loss(			'Lossrate vs Queue Size (mixed flows)', 					'loss_mixed.png',			mixed)
-		self.plot_variance(		'Relative Standard Deviation vs Queue Size (mixed flows)', 	'rsd_mixed.png',			mixed)
-		self.plot_fair(			'Jain Fairness vs RTT Unfairness (mixed flows)', 			'fairness_mixed.png',		mixed)
-		self.plot_fair_total(	'Jain Fairness (total) vs RTT Unfairness (mixed flows)', 	'fairness_total_mixed.png',	mixed)
-		self.plot_time_to_convergence('Convergence Time vs Fairness after Convergence (mixed flows)',	'convergence_mixed.png',	mixed)
-		self.plot_queueing_delay('Queuing Delay vs Queue Size (mixed flows)', 				'delay_mixed.png',			mixed)
+		scheme_a = mixed['scheme_a'].values[0]
+		scheme_b = mixed['scheme_b'].values[0]
+		scheme_str = "\n3x%s 3x%s competing"%(scheme_a, scheme_b)
+		self.plot_loss(			'Lossrate vs Queue Size' + scheme_str, 								'loss_mixed.png',			mixed)
+		self.plot_variance(		'Relative Standard Deviation vs Queue Size' + scheme_str,		 	'rsd_mixed.png',			mixed)
+		self.plot_fair(			'Jain Fairness vs RTT Unfairness' + scheme_str, 					'fairness_mixed.png',		mixed)
+		self.plot_fair_total(	'Jain Fairness (total) vs RTT Unfairness' + scheme_str, 			'fairness_total_mixed.png',	mixed)
+		self.plot_time_to_convergence('Convergence Time vs Fairness after Convergence' + scheme_str,'convergence_mixed.png',	mixed)
+		self.plot_queueing_delay('Queuing Delay vs Queue Size' + scheme_str,			 			'delay_mixed.png',			mixed)
 	
 	def plot_loss(self, title, filename, data):
 		fig, ax = plt.subplots()
@@ -105,7 +111,10 @@ class BenchmarkAnalysis():
 		grouped = filtered.groupby(['rtprop_a', 'bottleneck_rtprop'])
 		for k, v in dict(list(grouped)).items():
 			v = v.sort_values('q_size')
-			plt.semilogx(v['q_size'], v['mean_bottleneck_delay'] - v['bottleneck_rtprop'],label='%dms oneway propagation delay'%((k[0] + k[1])/2))
+			plt.semilogx(v['q_size'], v['mean_bottleneck_delay'] - (v['bottleneck_rtprop']/2),label='%dms oneway propagation delay'%((k[0] + k[1])/2))
+			q_sizes = v['q_size']
+			tputs = v['bottleneck_tput']
+		plt.semilogx(q_sizes, q_sizes/(tputs/8.0*1000), label='full queue', color='grey')
 		ax.set_xlabel('Queue Size (bytes)')
 		ax.set_ylabel('Queuing Delay (ms)')
 		plt.legend()
